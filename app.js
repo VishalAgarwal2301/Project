5a5dd602cf095afc59cf34990b4eac894de31ae8
VishalAgarwal2301 <vishalagarwallksec@gmail.com>
2023-10-30 21:43:54 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,53 +1,77 @@
-import express from "express";
-import { getData, putData } from "./database.js";
-const app=express()
-app.use(express.json())
-app.set("view engine","ejs")
-app.use(express.static("public"))
-app.use(express.urlencoded({extended:true}))
+import express from 'express';
+import bodyParser from 'body-parser';
+import request from 'request-promise';
+import { getData, putData } from './database.js';
 
-app.get('/',async(req,res)=>{
-    res.render("index.ejs")
-})
-//render
-app.get('/login',async(req,res)=>{
-    res.render("login.ejs")
-})
-app.post('/login',async(req,res)=>{
-    try {
-        const data=await getData()
-        const {username,pass}=req.body
-        data.forEach(e=>{
-            console.log(e.username,e.password)
-            console.log(username,pass)
-        })
-        res.send("<h1>Login Successful</h1>")
-    } catch (error) {
-        res.send("Failed")
-    }    
-})
+const app = express();
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/register',async(req,res)=>{
-    res.render("register.ejs")
-})
+app.get('/', async (req, res) => {
+  res.render('index.ejs');
+});
 
-app.post('/register',async(req,res)=>{
-    const {username,Email,pass}=req.body
-    try{
-        const data=await putData(username,Email,pass)
-        res.send("Successful")
+// render
+app.get('/login', async (req, res) => {
+  res.render('login.ejs');
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const data = await getData();
+    const { username, pass } = req.body;
+
+    data.forEach(e => {
+      if (username === e.username && pass === e.password) {
+        res.render('main.ejs');
+      }
+    });
+  } catch (error) {
+    res.send('Failed');
+  }
+});
+
+app.get('/register', async (req, res) => {
+  res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, Email, pass } = req.body;
+
+  // CAPTCHA verification
+  const userResponse = req.body['g-recaptcha-response'];
+  const secret = '6LdFS98oAAAAAHMxrA4lT79up7ZlzY8CXMJZZ9cu'; // Replace with your reCAPTCHA secret key
+
+  const verifyOptions = {
+    uri: 'https://www.google.com/recaptcha/api/siteverify',
+    method: 'POST',
+    json: true,
+    form: {
+      secret,
+      response: userResponse,
+    },
+  };
+
+  try {
+    const verifyResponse = await request(verifyOptions);
+    if (!verifyResponse.success) {
+      return res.send('CAPTCHA verification failed. Please try again.');
     }
-    catch{
-        res.send("failed")
-    }
-    // if(data){
-    //     res.render("Registration Successful")
-    // }
-    // else{
-    //     res.render("Failed")
-    // }
-})
+  } catch (error) {
+    return res.status(500).send('Error verifying CAPTCHA');
+  }
 
-app.listen(3000,()=>{
-    console.log("Server started")
-})
+  try {
+    // CAPTCHA verification passed, continue with user registration
+    const data = await putData(username, pass, Email);
+    res.render('login.ejs');
+  } catch {
+    res.send('Failed');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
